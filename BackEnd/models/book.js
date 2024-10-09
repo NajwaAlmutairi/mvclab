e041b123821d5f7e442dd2481c21f64c25f29090
najwa <NajwaBuayjan@gmail.com>
2024-10-09 14:59:30 +0300

    solving

---
+++ BackEnd/models/book.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+
+const book = new Schema ({
+    bookName: {
+        type: String,
+        required: true,
+      },
+      author: String,
+      editionNum:Number,
+      publishDate:String,
+      isOnlineV:Boolean,
+      prise:Number,
+      languages:Array,
+      classification:String,
+
+    },
+        {timestamps:true}
+    
+)
+
+const Book = mongoose.model('Book',book);
+
+export default Book;
